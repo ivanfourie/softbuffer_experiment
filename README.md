2c0369ebde315d8f538db4bde59ff972393a583f
Ivan Fourie <ivan.fourie@gmail.com>
2023-01-07 17:50:56 +0200

    Implement rate limiting

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # Introduction
 This project is a test of `winit` and `softbuffer` crates to implement a simple software frame buffer in a cross platform way. 
 
+# Frame rate limiting mechanism
+The `TICKS_PER_SECOND` constant determines the target frame rate, and the `SKIP_TICKS` constant determines the length of each tick in nanoseconds. 
+The `MAX_FRAMESKIP` constant sets the maximum number of frames that can be skipped in a single frame.
+
+Inside the event loop, a variable called `next_game_tick` is initialized to the current time (`Instant::now()`).
+
+A loop is used to update the world state until the current time is greater than the next game tick time (`next_game_tick`) or the number of loops 
+that have run so far (`loops`) is equal to the maximum number of frames to skip (`MAX_FRAMESKIP`).
+
+Inside the loop, the world is updated (`world.update()`) and the next game tick time is incremented by the length of a single tick (`Duration::new(0, SKIP_TICKS as u32)`). 
+The loop counter (`loops`) is also incremented.
+
+When the event loop receives a redraw request for the window (`Event::RedrawRequested`), the game world is rendered (`world.render(&mut display)`) and 
+the buffer is set as the window's buffer (`graphics_context.set_buffer(display.color_buffer(), width as u16, height as u16)`).
+
 ## Install OS dependencies
 https://bevyengine.org/learn/book/getting-started/setup/
--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,15 @@ mod world;
 use display::Display;
 use world::World;
 
+use std::time::{Duration, Instant};
 use softbuffer::GraphicsContext;
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::WindowBuilder;
+
+const TICKS_PER_SECOND: u64 = 75;
+const SKIP_TICKS: u64 = 1_000_000_000 / TICKS_PER_SECOND;
+const MAX_FRAMESKIP: u32 = 5;
 
 fn main() {
     // Create the event loop and window
@@ -25,12 +30,19 @@ fn main() {
     let mut display = Display::new(width, height).unwrap();
     let mut world = World::new().unwrap();
 
-    // Set up the event loop to run indefinitely
+    let mut next_game_tick = Instant::now();
+    
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Poll;
 
-        // Update the world
-        world.update();
+        // Make sure the world state is updated at a fixed rate
+        let mut loops: u32 = 0;
+        while Instant::now() > next_game_tick && loops < MAX_FRAMESKIP {  
+            // Update the world
+            world.update();
+            next_game_tick += Duration::new(0, SKIP_TICKS as u32);
+            loops += 1;
+        } 
 
         // Match on the event value
         match event {
@@ -39,11 +51,13 @@ fn main() {
             }
             // If the event is a redraw request for the window...
             Event::RedrawRequested(window_id) if window_id == window.id() => {
+                
                 // Render the world
                 world.render(&mut display);
 
                 // Set the buffer as the window's buffer
                 graphics_context.set_buffer(display.color_buffer(), width as u16, height as u16);
+                
             }
             // If the event is a close request for the window...
             Event::WindowEvent {
